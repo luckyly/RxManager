acbab81a04513777e506656fbcd36154877bb8dc
georgeci <georgeci007@gmail.com>
2015-10-13 13:40:04 +0300

    init and create first RxManger(only sync subscribe)

--- app/src/main/java/com/georgeci/coreback/MainActivity.java
+++ app/src/main/java/com/georgeci/coreback/MainActivity.java
@@ -1,13 +1,66 @@
 package com.georgeci.coreback;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.georgeci.coreback.i.SCommand;
+import com.georgeci.coreback.rx.RxWorkManager;
+import com.trello.rxlifecycle.components.support.RxAppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends RxAppCompatActivity {
+
+    RxWorkManager manager = new RxWorkManager();
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @OnClick(R.id.btn)
+    void click() {
+        SCommand command = new SCommand("awe");
+        manager.rxExecute(command).subscribe(
+                s -> Log.i("CSUBS", s),
+                throwable -> Log.i("CSUBS", "qw"),
+                () -> Log.i("CSUBS", "complete"));
+    }
+
+    @OnClick(R.id.btn2)
+    void click2() {
+
+    }
+
+    @OnClick(R.id.btn3)
+    void click3() {
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("TAG", "onPause: " + hashCode());
+        manager.shouldStop();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        manager.start(this);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
